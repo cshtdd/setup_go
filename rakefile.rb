a4b85db60c1c63a222357114fb76e9fe63d0c616
camilin87 <camilin87@gmail.com>
2016-02-05 22:25:07 -0500

    minor refactor

--- rakefile.rb
+++ rakefile.rb
@@ -2,11 +2,7 @@ task :default => :setup
 
 task :setup do
     go_path = read_variable "What do you want your $GOPATH to be? e.g. $HOME/devroot/go", "$HOME/devroot/go"
-    username = read_variable "Enter your github username e.g. codingdog", "user"
-
-    dir_to_create = "#{go_path}/src/github.com/#{username}"
-    puts "Creating directory #{dir_to_create}"
-    sh "mkdir -p #{dir_to_create}"
+    create_source_folder go_path
 end
 
 task :debug do
@@ -19,4 +15,12 @@ def read_variable(message, default_value)
     user_input = STDIN.gets.chomp
     user_input = default_value unless user_input.length > 0
     user_input
+end
+
+def create_source_folder(go_path)
+    username = read_variable "Enter your github username e.g. codingdog", "user"
+
+    dir_to_create = "#{go_path}/src/github.com/#{username}"
+    puts "Creating directory #{dir_to_create}"
+    sh "mkdir -p #{dir_to_create}"
 end
