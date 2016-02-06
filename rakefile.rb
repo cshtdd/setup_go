8f4f5904f38687153d0bb7484b384f97cbd5c8ea
camilin87 <camilin87@gmail.com>
2016-02-05 22:11:07 -0500

    read the user input

---
+++ rakefile.rb
@@ -0,0 +1,16 @@
+task :default => :setup
+
+task :setup do
+    go_path = read_variable "What do you want your $GOPATH to be? e.g. $HOME/devroot/go", "$HOME/devroot/go"
+    username = read_variable "Enter your github username", "user"
+
+    dir_to_create = "#{go_path}/src/github.com/#{username}"
+    puts "Creating directory #{dir_to_create}"
+end
+
+def read_variable(message, default_value)
+    puts message
+    user_input = STDIN.gets.chomp
+    user_input = default_value unless user_input.length > 0
+    user_input
+end
